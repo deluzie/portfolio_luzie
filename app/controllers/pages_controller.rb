2c97cb0bfe6aa87cac4fd0ef82ba8968099ecf85
Luzie Deubel <luzie@deubels.de>
2021-02-26 11:14:05 +0100

    added routes and controller actions for static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,13 @@ class PagesController < ApplicationController
   def home
     @projects = Project.all
   end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def legal_notice
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'projects#index'
   resources :projects, only: [:show, :index, :new, :create] do
   end
+  get "/contact", to: "pages#contact"
+  get "/about", to: "pages#about"
+  get "/legal-notice", to: "pages#legal_notice"
 end
