e200dee5410808cc6410d0789b5c1bbe761b4c04
Luzie Deubel <luzie@deubels.de>
2021-02-26 18:03:42 +0100

    changed mailer configurations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,7 @@
 class Contact < MailForm::Base
-  attribute :from_name, validate: true
-  attribute :from_email, validate: /\A[^@\s]+@[^@\s]+\z/i
-  attribute :subject, validate: true
-  attribute :message
+  attribute :name, validate: true
+  attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message, validate: true
   attribute :nickname, captcha: true
 
   # Declare the e-mail headers. It accepts anything the mail method
